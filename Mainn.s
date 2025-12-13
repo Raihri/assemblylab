2719fed07bb0e5050c63e7c062eb49ebed6f69e1
Salwa Baki <emmeryxanos@gmail.com>
2025-12-13 15:58:21 +0600

    Implement main loop for time simulation and checks
    Added main loop to simulate time passing and check medicine.

--- Mainn.s
+++ Mainn.s
@@ -11,8 +11,19 @@ main
         BL Init_Patients
         BL Vital_Alert_Handler
         BL Medicine_Scheduler
+main_loop
+         ; simulate time passing
+    LDR R0, =CURRENT_TIME
+    LDR R1, [R0]
+    ADD R1, R1, #1         ; increment by 1 unit per loop
+    STR R1, [R0]
 
+    ; check medicine
+    BL Medicine_Scheduler
+    B main_loop
+    
 Stop
         B Stop
 
         END
+
